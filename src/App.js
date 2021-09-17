536203852f0819b370453769654f72d556defc26
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2021-09-17 19:09:36 +0100

    Init react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import theme from './theme'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+
+// Pages
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+
 const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <div>Test</div>
+      <Router>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </Router>
     </ChakraProvider>
   )
 }
